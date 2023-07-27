ea0894e72f17d85e810b0860a79a79a01b3929e4
Katie Hull <katie.ann.hull@gmail.com>
2023-07-26 20:52:44 -0700

    Updating landing page to parse and debounce flavor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
+import { SetStateAction, useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import debounce from 'lodash.debounce'
+
+export function Recipe({ flavor }) {
+  return (
+    <div>
+      <h1>{flavor} Smoothie</h1>
+      <ul>
+        <li>Ingredient</li>
+        <li>Ingredient</li>
+      </ul>
+      <h3>Instructions</h3>
+      <ol>
+        <li>Mix it all together</li>
+        <li>Drink it</li>
+      </ol>
+    </div>
+      )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0);
+  const [flavor, setFlavor] = useState("");
+
+  const handleChange = (event) => {
+    setFlavor(event.target.value);
+  }
+
+  const debouncedHandleChange = useCallback(debounce(handleChange, 300), []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>          
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvgP-gCeQBovk0SNrVgT_nO45GTavOHXBy1S6Wz-Qm3Cj8ciQ&s" alt="Drink" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfEYBELmvJk4qPPFmC2j6CePyy6f5FYqymCz-Rj21ptR7Ni7E&s" alt="Pineapple" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Smoothiest</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <div>
+          <label>Enter a flavor for your smoothie</label>
+          <input type="text" onChange={debouncedHandleChange}></input>
+        </div>
+        <button> 
+          Make me a {flavor} smoothie
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Recipe flavor = {flavor}></Recipe>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
-
-export default App
+//also have AI name the smoothie?
+export default App;
